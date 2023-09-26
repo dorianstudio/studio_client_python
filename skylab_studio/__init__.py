8999aa61274ad5ac80cf3f3e2a94141a78a7b635
Eric Bailey <ebailey@dorianstudio.com>
2023-09-26 10:20:10 -0700

    Add public to path to match API docs

--- skylab_studio/__init__.py
+++ skylab_studio/__init__.py
@@ -5,17 +5,19 @@ For more information, visit https://studio.skylabtech.ai
 
 import json
 import logging
+
 import requests
 
 from .version import VERSION
 
-API_HEADER_KEY = 'X-SLT-API-KEY'
-API_HEADER_CLIENT = 'X-SLT-API-CLIENT'
+API_HEADER_KEY = "X-SLT-API-KEY"
+API_HEADER_CLIENT = "X-SLT-API-CLIENT"
 
-LOGGER = logging.getLogger('skylab_studio')
+LOGGER = logging.getLogger("skylab_studio")
 LOGGER.propagate = False
 
-class api: #pylint: disable=invalid-name
+
+class api:  # pylint: disable=invalid-name
     """
     The client for accessing the Skylab Studio platform.
 
@@ -31,11 +33,11 @@ class api: #pylint: disable=invalid-name
         debug (boolean): Whether or not to allow debugging information to be printed.
     """
 
-    api_proto = 'https'
-    api_port = '443'
-    api_host = 'studio.skylabtech.ai'
-    api_version = '1'
-    api_key = 'THIS_IS_A_TEST_API_KEY'
+    api_proto = "https"
+    api_port = "443"
+    api_host = "studio.skylabtech.ai"
+    api_version = "1"
+    api_key = "THIS_IS_A_TEST_API_KEY"
 
     debug = False
 
@@ -45,50 +47,44 @@ class api: #pylint: disable=invalid-name
 
         self.api_key = api_key
 
-        if 'api_host' in kwargs:
-            self.api_host = kwargs['api_host']
-        if 'api_proto' in kwargs:
-            self.api_proto = kwargs['api_proto']
-        if 'api_port' in kwargs:
-            self.api_port = kwargs['api_port']
-        if 'api_version' in kwargs:
-            self.api_version = kwargs['api_version']
-        if 'debug' in kwargs:
-            self.debug = kwargs['debug']
+        if "api_host" in kwargs:
+            self.api_host = kwargs["api_host"]
+        if "api_proto" in kwargs:
+            self.api_proto = kwargs["api_proto"]
+        if "api_port" in kwargs:
+            self.api_port = kwargs["api_port"]
+        if "api_version" in kwargs:
+            self.api_version = kwargs["api_version"]
+        if "debug" in kwargs:
+            self.debug = kwargs["debug"]
 
         if self.debug:
-            logging.basicConfig(format='%(asctime)-15s %(message)s', level=logging.DEBUG)
+            logging.basicConfig(
+                format="%(asctime)-15s %(message)s", level=logging.DEBUG
+            )
 
-            LOGGER.debug('Debug enabled')
+            LOGGER.debug("Debug enabled")
             LOGGER.propagate = True
 
     def _build_http_auth(self):
-        return (self.api_key, '')
+        return (self.api_key, "")
 
     @staticmethod
     def _build_request_headers():
-        client_header = '%s-%s' % (
-            'python',
-            VERSION
-        )
+        client_header = "%s-%s" % ("python", VERSION)
 
         headers = {
             API_HEADER_CLIENT: client_header,
-            'Content-type': 'application/json',
-            'Accept': 'text/plain'
+            "Content-type": "application/json",
+            "Accept": "text/plain",
         }
 
         return headers
 
     def _build_request_path(self, endpoint):
-        path = '/api/v%s/%s' % (self.api_version, endpoint)
+        path = "/api/public/v%s/%s" % (self.api_version, endpoint)
 
-        path = "%s://%s:%s%s" % (
-            self.api_proto,
-            self.api_host,
-            self.api_port,
-            path
-        )
+        path = "%s://%s:%s%s" % (self.api_proto, self.api_host, self.api_port, path)
 
         return path
 
@@ -101,168 +97,111 @@ class api: #pylint: disable=invalid-name
 
     def _api_request(self, endpoint, http_method, **kwargs):
         """Private method for api requests"""
-        LOGGER.debug(' > Sending API request to endpoint: %s', endpoint)
+        LOGGER.debug(" > Sending API request to endpoint: %s", endpoint)
 
         auth = self._build_http_auth()
 
         headers = self._build_request_headers()
-        LOGGER.debug('\theaders: %s', headers)
+        LOGGER.debug("\theaders: %s", headers)
 
         path = self._build_request_path(endpoint)
-        LOGGER.debug('\tpath: %s', path)
+        LOGGER.debug("\tpath: %s", path)
 
-        data = self._build_payload(kwargs.get('payload'))
+        data = self._build_payload(kwargs.get("payload"))
         if not data:
-            data = kwargs.get('data')
-        LOGGER.debug('\tdata: %s', data)
+            data = kwargs.get("data")
+        LOGGER.debug("\tdata: %s", data)
 
         req_kw = dict(
             auth=auth,
             headers=headers,
         )
 
-        if http_method == 'POST':
+        if http_method == "POST":
             if data:
                 response = requests.post(path, data=data, **req_kw)
             else:
                 response = requests.post(path, **req_kw)
-        elif http_method == 'PUT':
+        elif http_method == "PUT":
             response = requests.put(path, data=data, **req_kw)
-        elif http_method == 'DELETE':
+        elif http_method == "DELETE":
             response = requests.delete(path, **req_kw)
         else:
             response = requests.get(path, **req_kw)
 
-        LOGGER.debug('\tresponse code:%s', response.status_code)
+        LOGGER.debug("\tresponse code:%s", response.status_code)
 
         try:
-            LOGGER.debug('\tresponse: %s', response.json())
+            LOGGER.debug("\tresponse: %s", response.json())
         except ValueError:
-            LOGGER.debug('\tresponse: %s', response.content)
+            LOGGER.debug("\tresponse: %s", response.content)
 
         return response
 
     def list_jobs(self):
-        """ API call to get all jobs """
-        return self._api_request(
-            'jobs',
-            'GET'
-        )
+        """API call to get all jobs"""
+        return self._api_request("jobs", "GET")
 
     def create_job(self, payload=None):
-        """ API call to create a job """
-        return self._api_request(
-            'jobs',
-            'POST',
-            payload=payload
-        )
+        """API call to create a job"""
+        return self._api_request("jobs", "POST", payload=payload)
 
     def get_job(self, job_id):
-        """ API call to get a specific job """
-        return self._api_request(
-            'jobs/%s' % job_id,
-            'GET'
-        )
+        """API call to get a specific job"""
+        return self._api_request("jobs/%s" % job_id, "GET")
 
     def update_job(self, job_id, payload=None):
-        """ API call to update a specific job """
-        return self._api_request(
-            'jobs/%s' % job_id,
-            'PUT',
-            payload=payload
-        )
+        """API call to update a specific job"""
+        return self._api_request("jobs/%s" % job_id, "PUT", payload=payload)
 
     def delete_job(self, job_id):
-        """ API call to delete a specific job """
-        return self._api_request(
-            'jobs/%s' % job_id,
-            'DELETE'
-        )
+        """API call to delete a specific job"""
+        return self._api_request("jobs/%s" % job_id, "DELETE")
 
     def process_job(self, job_id):
-        """ API call to process a specific job """
-        return self._api_request(
-            'jobs/%s/process' % job_id,
-            'POST'
-        )
+        """API call to process a specific job"""
+        return self._api_request("jobs/%s/process" % job_id, "POST")
 
     def cancel_job(self, job_id):
-        """ API call to cancel a specific job """
-        return self._api_request(
-            'jobs/%s/cancel' % job_id,
-            'POST'
-        )
+        """API call to cancel a specific job"""
+        return self._api_request("jobs/%s/cancel" % job_id, "POST")
 
     def list_profiles(self):
-        """ API call to get all profiles """
-        return self._api_request(
-            'profiles',
-            'GET'
-        )
+        """API call to get all profiles"""
+        return self._api_request("profiles", "GET")
 
     def create_profile(self, payload=None):
-        """ API call to create a profile """
-        return self._api_request(
-            'profiles',
-            'POST',
-            payload=payload
-        )
+        """API call to create a profile"""
+        return self._api_request("profiles", "POST", payload=payload)
 
     def get_profile(self, profile_id):
-        """ API call to get a specific profile """
-        return self._api_request(
-            'profiles/%s' % profile_id,
-            'GET'
-        )
+        """API call to get a specific profile"""
+        return self._api_request("profiles/%s" % profile_id, "GET")
 
     def update_profile(self, profile_id, payload=None):
-        """ API call to update a specific profile """
-        return self._api_request(
-            'profiles/%s' % profile_id,
-            'PUT',
-            payload=payload
-        )
+        """API call to update a specific profile"""
+        return self._api_request("profiles/%s" % profile_id, "PUT", payload=payload)
 
     def delete_profile(self, profile_id):
-        """ API call to delete a specific profile """
-        return self._api_request(
-            'profiles/%s' % profile_id,
-            'DELETE'
-        )
+        """API call to delete a specific profile"""
+        return self._api_request("profiles/%s" % profile_id, "DELETE")
 
     def list_photos(self):
-        """ API call to get all photos """
-        return self._api_request(
-            'photos',
-            'GET'
-        )
+        """API call to get all photos"""
+        return self._api_request("photos", "GET")
 
     def create_photo(self, payload=None):
-        """ API call to create a photo """
-        return self._api_request(
-            'photos',
-            'POST',
-            payload=payload
-        )
+        """API call to create a photo"""
+        return self._api_request("photos", "POST", payload=payload)
 
     def get_photo(self, photo_id):
-        """ API call to get a specific photo """
-        return self._api_request(
-            'photos/%s' % photo_id,
-            'GET'
-        )
+        """API call to get a specific photo"""
+        return self._api_request("photos/%s" % photo_id, "GET")
 
     def update_photo(self, photo_id, payload=None):
-        """ API call to update a specific photo """
-        return self._api_request(
-            'photos/%s' % photo_id,
-            'PUT',
-            payload=payload
-        )
+        """API call to update a specific photo"""
+        return self._api_request("photos/%s" % photo_id, "PUT", payload=payload)
 
     def delete_photo(self, photo_id):
-        """ API call to delete a specific photo """
-        return self._api_request(
-            'photos/%s' % photo_id,
-            'DELETE'
-        )
+        """API call to delete a specific photo"""
+        return self._api_request("photos/%s" % photo_id, "DELETE")
